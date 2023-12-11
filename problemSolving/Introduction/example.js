55231090f3c8b1993f94bf011244c81b60b6dde9
unknown <saikrishana9@gmail.com>
2023-12-11 10:52:47 +0530

    add example file

---
+++ problemSolving/Introduction/example.js
@@ -0,0 +1,14 @@
+let array = [1, 2, 3, 2, 1, 3, 4, 5];
+// Output: { "1": 2, "2": 2, "3": 2, "4": 1, "5": 1 }
+function countOccurrence(arr) {
+    let result = {};
+    for(let ele of arr) {
+        if(result.hasOwnProperty(ele)) {
+            debugger; 
+            ele;
+        }
+
+
+        33
+    }
+}
